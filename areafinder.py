65d512bc055df9139808e5f457503aa3bac3088f
Anthony V. Naut <83789702+anthonynaut@users.noreply.github.com>
2021-06-04 00:45:26 -0400

    Am I behind on something?
    Struggled.

---
+++ areafinder.py
@@ -0,0 +1,9 @@
+name = "Formula"
+def multiply(num1, num2):
+    return num1 * num2
+    
+    # c = 2 pi r 
+Pi = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = Pi * r * r
+print("Area of a circle = %.2f" %area)
